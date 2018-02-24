ab050fb8f59e1d2495d2e55839cd98e7a0af80dd
Septenary <sevenfm2@gmail.com>
2018-02-24 12:01:59 +0000

    16 Channel Support, New Library for GPIO
    Switched out node rpio for array-gpio, cleaned up sloppy code

--- rc.js
+++ rc.js
@@ -3,10 +3,7 @@ var url = require('url')
 var fs = require('fs')
 var path = require('path')
 var baseDirectory = __dirname
-var rpio = require('rpio')
-
-
-rpio.init({mapping: 'gpio'});
+var r = require('array-gpio')
 
 var port = 8000
 
@@ -33,39 +30,29 @@ var server = http.createServer(function (request, response) {
    }
 });
 
-pinList = [2, 3, 4, 17, 27, 22, 10, 9];
-
-for (var i; i < pinList.length; i++){
-	rpio.open(pinList[i], rpio.OUTPUT, rpio.HIGH);
-}
-
+var pinOut = r.out(3, 5, 7, 11, 13, 15, 19, 21, 8, 10, 12, 16, 18, 22, 24, 26);
+for (var i = 0; i < pinOut.length; i++){
+if (i%2==0){
+pinOut[i].write(1);
+}};
 
 var togglePin = (function(pin){
-	state = 1-(rpio.read(pinList[pin-1]));
-	if (state){
-	rpio.write(pinList[pin-1], rpio.HIGH);
-	} else {
-	rpio.write(pinList[pin-1], rpio.LOW);
-	}
-	console.log("Relay " +pin+ ": " + (1-state));
-})
+	state = pinOut[pin].read(() => {
+	pinOut[pin].write(1-state)
+	console.log("Relay " +pin+ ": " + state);
+})})
 
 var io = require('socket.io')(server);
 server.listen(port);
 console.log('Server @ 10.0.0.23: '+port);
 io.on('connect', function(client) { 
 	console.log('Client connected...'); 
-	io.emit('pinUpdate', 1, 1-(rpio.read(pinList[0])));
-	io.emit('pinUpdate', 2, 1-(rpio.read(pinList[1])));
-	io.emit('pinUpdate', 3, 1-(rpio.read(pinList[2])));
-	io.emit('pinUpdate', 4, 1-(rpio.read(pinList[3])));
-	io.emit('pinUpdate', 5, 1-(rpio.read(pinList[4])));
-	io.emit('pinUpdate', 6, 1-(rpio.read(pinList[5])));
-	io.emit('pinUpdate', 7, 1-(rpio.read(pinList[6])));
-	io.emit('pinUpdate', 8, 1-(rpio.read(pinList[7])));
+	for (var i = 1; i < 16; i++ ){
+	io.emit('pinUpdate', i, 1-pinOut[i].read());
+	};
 	console.log('Clients updated...')
     	client.on('togglePin', function(pin) {
     		togglePin(pin);
-		io.emit('pinUpdate', pin, 1-(rpio.read(pinList[pin-1])));
+		io.emit('pinUpdate', pin, pinOut[pin].read());
     });
 });
