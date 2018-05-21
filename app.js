dec27ed00ab1acf522e3285d73ace1f281a98cdc
Septenary <sevenfm2@gmail.com>
2018-05-21 10:02:52 -0400

    Testing I2C

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var url = require('url')
 var fs = require('fs')
 var path = require('path')
 var baseDirectory = __dirname
-var r = require('array-gpio')
-
+var raspi = require('raspi')
+var I2C = require('raspi-i2c')
 var port = 80
 
 var server = http.createServer(function (request, response) {
-    try {
+    try { 
         var requestUrl = url.parse(request.url)
 
         // need to use path.normalize so people can't access directories underneath baseDirectory
@@ -35,48 +35,51 @@ var server = http.createServer(function (request, response) {
         console.log(e.stack)
    }
 });
+init(() => {
+   const i2c = new I2C();
+    console.log(i2c.readByteSync(0x20));
+});
 
-var pinOut = r.out(3,5,7,10,12,16,13,15);
 var clearPins = function(){
     for (var i = 0; i < pinOut.length; i++){
     pinOut[i].write(1);
 }};
 clearPins();
 
-var clientUpdate = function (){
-    for (var i = 0; i < pinOut.length; i++ ){
-    io.emit('pinUpdate', i, 1-pinOut[i].read());
-}};
-
-var togglePin = function(){
-	var k = arguments[0]
-	for (var j = 0; j < k.length; j++){
-		state = pinOut[k[j]].read()
-		pinOut[k[j]].write(1-state)
-	 	//console.log("Relay " +(k[j]+1)+ ": " + state);
-	}
-    	clientUpdate();
-};
-
-var io = require('socket.io')(server);
-server.listen(port);
-console.log('Server @ 10.0.0.16:'+port);
-io.on('connect', function(client) { 
-    console.log('Client connected...'); 
-    clientUpdate();
-
-    client.on('togglePin', function(data) {
-	//console.log(data)
-        togglePin(data);
-    });
-
-    client.on('clearPins', function(pin) {
-        clearPins();
-   	clientUpdate();
-    });
-
-    setInterval(clientUpdate, 500);
-
-});
+//var clientUpdate = function (){
+//    for (var i = 0; i < pinOut.length; i++ ){
+//    io.emit('pinUpdate', i, 1-pinOut[i].read());
+//}};
+//
+//var togglePin = function(){
+//	var k = arguments[0]
+//	for (var j = 0; j < k.length; j++){
+//		state = pinOut[k[j]].read()
+//		pinOut[k[j]].write(1-state)
+//	 	//console.log("Relay " +(k[j]+1)+ ": " + state);
+//	}
+//    	clientUpdate();
+//};
+//
+//var io = require('socket.io')(server);
+//server.listen(port);
+//console.log('Server @ 10.0.0.16:'+port);
+//io.on('connect', function(client) { 
+//    console.log('Client connected...'); 
+//    clientUpdate();
+//
+//    client.on('togglePin', function(data) {
+//	//console.log(data)
+//        togglePin(data);
+//    });
+//
+//    client.on('clearPins', function(pin) {
+//        clearPins();
+//   	clientUpdate();
+//    });
+//
+//    setInterval(clientUpdate, 500);
+//
+//});
 
 
